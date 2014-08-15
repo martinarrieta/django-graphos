17a8c1667bec5a323ca0d3e2a6829e8152e37c10
Martin Arrieta <mail@martinarrieta.me>
2014-08-14 23:22:52 -0300

    datetime support

--- graphos/renderers/highcharts.py
+++ graphos/renderers/highcharts.py
@@ -1,9 +1,8 @@
 from .base import BaseChart
 import json
-
+from time import mktime
 
 from django.template.loader import render_to_string
-
 
 class BaseHighCharts(BaseChart):
     def get_template(self):
@@ -24,6 +23,54 @@ class BaseHighCharts(BaseChart):
         return self.get_data()[0][0]
 
 
+class DateTimeLineChart(BaseHighCharts):
+
+    def get_chart_type(self):
+        return "line"
+
+    def get_categories(self):
+        return None
+
+    def get_series(self):
+        tmpdata = self.get_data()
+
+        tzoffset = self.get_tzoffset()
+
+        data=[tmpdata[0]]
+        for d in tmpdata[1:]:
+            data.append([datetime_to_timestamp(d[0]) + tzoffset * 1000 , d[1] ])
+
+        print data
+        series_names = data[0][1:]
+        serieses = []
+        for i, name in enumerate(series_names):
+            serieses.append({"name": name, "data": data[1:]})
+        return json.dumps(serieses)
+    
+    def get_x_axis_datetime_format(self, formats=None):
+        
+        x_axis_format = {
+            'millisecond': '%H:%M:%S.%L',
+            'second': '%H:%M:%S',
+            'minute': '%H:%M',
+            'hour': '%H:%M',
+            'day': '%e. %b',
+            'week': '%e. %b',
+            'month': '%b \'%y',
+            'year': '%Y'
+        }
+        if formats is not None:
+            x_axis_format = formats
+        return json.dumps(x_axis_format)
+
+    def get_x_axis_type(self):
+        return 'datetime'
+
+    def get_tzoffset(self):
+        try:
+            return self.context_data['tzoffset']
+        except:
+            return 0
 
 class LineChart(BaseHighCharts):
     def get_chart_type(self):
@@ -47,3 +94,7 @@ class PieChart(BaseHighCharts):
 
 def column(matrix, i):
     return [row[i] for row in matrix]
+
+
+def datetime_to_timestamp(dt):
+    return int(mktime(dt.timetuple())*1000)
